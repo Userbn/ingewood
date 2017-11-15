96340faebf1073df6a7c88f95b259d7d696380e0
Benjamin Descamps <userbn@gmail.com>
2017-11-15 22:31:35 +0100

    update images

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
 var http = require('http');
 var path = require('path');
 
@@ -10,17 +5,13 @@ var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
 var router = express();
 var server = http.createServer(router);
 var io = socketio.listen(server);
 
 router.use(express.static(path.resolve(__dirname, 'client')));
+
+console.log(router.use(express.static(path.resolve(__dirname, 'client'))));
 var messages = [];
 var sockets = [];
 
@@ -80,5 +71,4 @@ function broadcast(event, data) {
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
